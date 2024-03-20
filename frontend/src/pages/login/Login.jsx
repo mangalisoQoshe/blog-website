0024dc1fe64cd2b78235d3e0ca634f762ad13a85
mangaliso <michael.qoshe98@gmail.com>
2024-03-20 17:20:33 +0200

    use firebabse to implement user login

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,82 @@
+import { useState } from "react";
+import { auth } from "../../services/firebase/config";
+import {signInWithEmailAndPassword,signOut} from  "firebase/auth"
+
+const Login = () => {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
 
 
-function Login() {
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (input.username !== "" && input.password !== "") {
+      signInWithEmailAndPassword(auth,input.email,input.password)
+      .then((userCredentail)=>{
+        const user = userCredentail.user
+        console.log(user)
+      })
+      .catch((err)=>{
+        console.log(err.message)
+      })
+      return;
+    }
+    alert("please provide a valid input");
+  };
+
+  const handleInput = (e) => {
+  
+    const { name, value } = e.target;
+    setInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+   
+  };
+
+  const handleSignOut=()=>{
+    signOut(auth)
+    .then(()=>{
+      console.log("signed out successfully")
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
   return (
-    <div>Login</div>
-  )
-}
+    <form onSubmit={handleLogin}>
+      <div className="form_control">
+        <label htmlFor="user-email">Email:</label>
+        <input
+          type="email"
+          id="user-email"
+          name="email"
+          aria-describedby="user-email"
+          aria-invalid="false"
+          required
+          onChange={handleInput}
+        />
+    
+      </div>
+      <div className="form_control">
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          aria-describedby="user-password"
+          aria-invalid="false"
+          onChange={handleInput}
+        />
+     
+      </div>
+      <button className="btn-submit">Login</button>
+    </form>
+  );
+};
 
-export default Login
+export default Login;
---
+++ frontend/src/services/firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjyxPWHZ3ficnNxG917LiEZKuBLTD1on4",
+  authDomain: "sams-blog-b8611.firebaseapp.com",
+  projectId: "sams-blog-b8611",
+  storageBucket: "sams-blog-b8611.appspot.com",
+  messagingSenderId: "552491096001",
+  appId: "1:552491096001:web:7a63f4cf52d0fa3973c140",
+  measurementId: "G-PFNLY28FKY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+export const auth = getAuth(app)
