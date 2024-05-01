c8774c02312a975f27c16d11696e9eb3cc4082d0
mangaliso <michael.qoshe98@gmail.com>
2024-05-01 22:24:24 +0200

    add hamburgerButton

---
+++ frontend/src/components/hamburger/HamburgerButton.jsx
@@ -0,0 +1,22 @@
+import styles from "./Hamburger.module.css"; // Import CSS for styling hamburger button
+
+
+const HamburgerButton = ({isOpen,toggleMenu}) => {
+
+
+  return (
+    <div className={styles["hamburger-menu"]}>
+      <button
+        className={`${styles["hamburger-button"]} ${isOpen ? styles.open : ''}`} // Apply conditional class from styles object
+        onClick={toggleMenu}
+      >
+        <div className={styles.line} />
+        <div className={styles.line} />
+        <div className={styles.line} />
+      </button>
+    </div>
+  );
+};
+
+export default HamburgerButton;
+
--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,16 +1,21 @@
 import styles from "./Nav.module.css";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 //component imports
 
 import { MoonIcon } from "../icons/Icons";
 import ActiveLink from "../active-link/ActiveLink";
 import useAuth from "../../context/authContext/useAuth";
-
-import Hamburger from "../hamburger/Hamburger";
+import HamburgerButton from "../hamburger/HamburgerButton";
 
 function Nav() {
   const { currentUser } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((open) => !open);
+  };
 
   return (
     <div className={styles.nav}>
@@ -18,21 +23,20 @@ function Nav() {
         S. Mathangana
       </NavLink>
 
-      <Hamburger>
-        <div className={`${styles.stuff}`}>
-          <ActiveLink to="/">Home</ActiveLink>
-          <ActiveLink to="/blog">Blog</ActiveLink>
-          {currentUser ? (
-            <NavLink to="/create-blog" className={styles.link}>
-              Create Blog
-            </NavLink>
-          ) : null}
+      <div className={`${styles.menu}${isOpen ? "open" : ""}`}>
+        <ActiveLink to="/">Home</ActiveLink>
+        <ActiveLink to="/blog">Blog</ActiveLink>
+        {currentUser ? (
+          <NavLink to="/create-blog" className={styles.link}>
+            Create Blog
+          </NavLink>
+        ) : null}
 
-          <button>
-            <MoonIcon />
-          </button>
-        </div>
-      </Hamburger>
+        <button>
+          <MoonIcon />
+        </button>
+      </div>
+      <HamburgerButton isOpen={isOpen} toggleMenu={toggleMenu} />
     </div>
   );
 }
